053f26726bbf50b016add31b60daeb330c6ee816
Tom Beadman <tom.beadman@rentalresult.com>
2013-10-03 17:21:49 +0100

    Setting up libgdx structure - CRLF conflict

---
+++ src/tomb/unit001/Main.java
@@ -0,0 +1,16 @@
+package tomb.unit001;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+  public static void main(String[] args) {
+    LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+    cfg.title = "Unit001";
+    cfg.useGL20 = false;
+    cfg.width = 480;
+    cfg.height = 320;
+
+    new LwjglApplication(new Unit001(), cfg);
+  }
+}
